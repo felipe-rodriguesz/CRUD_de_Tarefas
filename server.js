5764c7182360dc33a811f67cdeea68126af5eea8
Felipe Rodrigues <feliperodrigues.2004@alunos.utfpr.edu.br>
2026-07-11 14:38:58 -0300

    feat: conclusao da etapa 3 - rota POST usando streams e JSON

--- server.js
+++ server.js
@@ -13,9 +13,27 @@ const server = http.createServer((request, response) => {
     if (metodo === 'GET' && url === '/tasks') {
         response.writeHead(200, { "content-type": "application/json"});
         const tarefasEmTexto = JSON.stringify(db.tarefas);
-response.end(tarefasEmTexto);
-    }else{
-        response.writeHead(404, {"content-type":"text/plain"})
+        response.end(tarefasEmTexto);
+
+    } else if (metodo === 'POST' && url === '/tasks') {
+        let pedacosDoBody = [];
+
+        request.on('data', (pedaco) => {
+            pedacosDoBody.push(pedaco);
+        });
+
+        request.on('end', () => {
+            const bodyEmTexto = Buffer.concat(pedacosDoBody).toString();
+            const objetoRecebido = JSON.parse(bodyEmTexto);
+            
+            createTask(objetoRecebido.titulo, objetoRecebido.descricao);
+
+            response.writeHead(201, { "content-type": "text/plain"});
+            response.end("Tarefa criada com sucesso via POST!");
+        });
+
+    } else {
+        response.writeHead(404, { "content-type": "text/plain" })
         response.end("Erro 404: Rota não encontrada");
     }
 
