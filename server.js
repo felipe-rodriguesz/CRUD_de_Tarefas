a8463b9a67e5c29de6b9239b2f562bf4e87469aa
Felipe Rodrigues <feliperodrigues.2004@alunos.utfpr.edu.br>
2026-07-11 16:39:21 -0300

    feat: conclusao final da Etapa 4 - CRUD completo e padrao REST

--- server.js
+++ server.js
@@ -6,16 +6,39 @@ createTask("Estudar JavaScript", "Fazer o CRUD funcionar");
 createTask("Estudar Node.js", "Verificar se a lista de tarefas está sendo gerada");
 createTask("Estudar HTTP", "Entender como o JSON trafega na rede");
 
-const server = http.createServer((request, response) => {
-    const url = request.url;
-    const metodo = request.method;
+const regexID = /^\/tasks\/(?<id>[0-9]+)/;
 
-    if (metodo === 'GET' && url === '/tasks') {
+const server = http.createServer((request, response) => {
+    const url = new URL(request.url, `http://${request.headers.host}`);
+    const caminho = url.pathname;
+    const metodo = request.method;
+    const urlID = caminho.match(regexID);
+
+    let idTarefa = null;
+    if (urlID) {
+        idTarefa = Number(urlID.groups.id);
+    }
+
+    if (metodo === 'GET' && caminho === '/tasks') {
+        const pesquisa = url.searchParams.get('search');
+
+        let tarefasFiltradas = db.tarefas;
+        
+        if (pesquisa){
+            tarefasFiltradas = db.tarefas.filter(tarefa => {
+                const tituloMinusculo = tarefa.titulo.toLowerCase();
+                const descricaoMinuscula = tarefa.descricao.toLowerCase();
+                const pesquisaMinuscula = pesquisa.toLowerCase();
+
+                return tituloMinusculo.includes(pesquisaMinuscula) || descricaoMinuscula.includes(pesquisaMinuscula);
+            });
+        }
+
         response.writeHead(200, { "content-type": "application/json"});
-        const tarefasEmTexto = JSON.stringify(db.tarefas);
+        const tarefasEmTexto = JSON.stringify(tarefasFiltradas);
         response.end(tarefasEmTexto);
 
-    } else if (metodo === 'POST' && url === '/tasks') {
+    } else if (metodo === 'POST' && caminho === '/tasks') {
         let pedacosDoBody = [];
 
         request.on('data', (pedaco) => {
@@ -25,13 +48,40 @@ const server = http.createServer((request, response) => {
         request.on('end', () => {
             const bodyEmTexto = Buffer.concat(pedacosDoBody).toString();
             const objetoRecebido = JSON.parse(bodyEmTexto);
-            
+
             createTask(objetoRecebido.titulo, objetoRecebido.descricao);
 
             response.writeHead(201, { "content-type": "text/plain"});
             response.end("Tarefa criada com sucesso via POST!");
         });
 
+    } else if (metodo === 'DELETE' && idTarefa !== null) {
+        deleteTask(idTarefa);
+        response.writeHead(200, { "content-type": "text/plain" });
+        response.end("Tarefa deletada com sucesso via DELETE!");
+        
+    } else if (metodo === 'PATCH' && idTarefa !== null && caminho.includes('/complete')) {
+        updateTask(idTarefa, {status: "CONCLUÍDO!"})
+        response.writeHead(200, { "content-type": "text/plain" })
+        response.end("Tarefa marcada como concluída via PATCH!");
+
+    } else if (metodo === 'PUT' && idTarefa !== null) {
+        let pedacosDoBody = [];
+        
+        request.on('data', (pedaco) => {
+            pedacosDoBody.push(pedaco);
+        });
+
+        request.on('end', () => {
+            const bodyEmTexto = Buffer.concat(pedacosDoBody).toString();
+            const objetoRecebido = JSON.parse(bodyEmTexto);
+
+            updateTask(idTarefa, objetoRecebido);
+
+            response.writeHead(200, { "content-type": "text/plain" });
+            response.end("Tarefa atualizada com sucesso via PUT!");
+        });
+        
     } else {
         response.writeHead(404, { "content-type": "text/plain" })
         response.end("Erro 404: Rota não encontrada");
